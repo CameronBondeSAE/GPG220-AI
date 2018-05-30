487fa9aa4346bac60144837a68a891ba2aa9d2a5
Unknown <rhysipod@gmail.com>
2018-05-30 14:28:52 +0800

    AI Controller initalize

--- Assets/Rhys Buszko/Scripts/Controller.cs
+++ Assets/Rhys Buszko/Scripts/Controller.cs
@@ -2,15 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Controller : MonoBehaviour {
+public class Controller : MonoBehaviour
+{
+    Health health;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+        health = GetComponent<Health>();
+
+        health.OnDeathEvent += OnDeath;
+    }
+
+    private void OnDeath()
+    {
+        print("I'm dead");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
