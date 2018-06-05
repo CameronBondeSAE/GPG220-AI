84040221428d5a2f880da432a384016a1d46da5b
Unknown <rhysipod@gmail.com>
2018-06-05 12:30:35 +0800

    Redo (test)

--- Assets/Rhys Buszko/Scripts/Controller.cs
+++ Assets/Rhys Buszko/Scripts/Controller.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour {
 
+    Health My_Health;
+    Body My_Body;
+
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        My_Health = GetComponent<Health>();
+        My_Health.OnDeathEvent += Death;
+        My_Body = GetComponent<Body>(); 
 	}
 	
 	// Update is called once per frame
-	void Update () {
+
+    private void Death()
+    {
+        My_Body.OnDeath();
+    }
+
+	void Update ()
+    {
 		
 	}
 }
