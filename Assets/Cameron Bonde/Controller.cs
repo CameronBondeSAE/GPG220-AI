841b843b83b649ce25004bfa024552a5cc53deea
Cameron Bonde <c.bonde@sae.edu>
2018-06-05 17:10:57 +1000

    Spawning and Mr Dude updates

---
+++ Assets/Cameron Bonde/Controller.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cam
+{
+	public class Controller : ControllerBase
+	{
+		public Model model;
+
+		public void Update()
+		{
+			if (Random.Range(0f, 10f) > 9.99f)
+				model.Ability1();
+			if (Random.Range(0f, 10f) > 9.99f)
+				model.Ability2();
+		}
+
+	}
+
+}
---
+++ Assets/Cameron Bonde/Model.cs
@@ -0,0 +1,24 @@
+﻿namespace Cam
+{
+	public class Model : CharacterBase
+	{
+		public float things;
+
+		public override void Ability1()
+		{
+			base.Ability1();
+
+			print("Merging");
+			debugText = "Merging";
+		}
+
+		public override void Ability2()
+		{
+			base.Ability2();
+
+			print("Split");
+			debugText = "Split";
+		}
+	}
+
+}
