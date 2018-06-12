e845aa0ae1eec9406c08da705f76fe0906cbfd81
Tristienhousen <1007934@student.sae.edu.au>
2018-06-12 23:54:59 +1000

    Trystin 12/6/2018 - Vision Agent V1.1
    Alterations:
        - Basic Vision Agent added.
            - Major optimization undertaken to reduce unneeded iteration and length/cost of call.
        - Basic Memory Agent Added

---
+++ Assets/Editor/Trystin/AIVisionTestInspector.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+namespace Trystin
+{
+    [CustomEditor(typeof(TestVisionAI))]
+    public class AIVisionTestInspector : Editor
+    {
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            TestVisionAI TargetTestMovementAI = (TestVisionAI)target;
+
+            if (GUILayout.Button("Test Vision Range"))
+            {
+                if (NodeManager.Instance.SetupCompletate)
+                    TargetTestMovementAI.SightRangeScan();
+                else
+                    Debug.Log("Wait for NodeManager Setup is compleate");
+            }
+        }
+    }
+}
--- Assets/Trystin Garrett/Scripts/AIScripts/TestVisionAI.cs
+++ Assets/Trystin Garrett/Scripts/AIScripts/TestVisionAI.cs
@@ -7,65 +7,104 @@ namespace Trystin
 {
     public class TestVisionAI : MonoBehaviour
     {
-
+        public List<Node> NodesInCurrentSight = new List<Node>();
         public bool[,] KnownNodes;
         public int VisionRange = 4;
 
+        public bool VisualKnowledgeDebugging = false;
+        public bool VisualSightDebugging = false;
+        public Vector2Int CurrentGridPos;
+
+        public float VisualUpdateTiming = 0;
+        public float VisualUpdateTimingInterval = 0.5f;
         // Use this for initialization
         void Start()
         {
             if (NodeManager.Instance != null)
-            {
                 KnownNodes = new bool[NodeManager.Instance.GridXLength, NodeManager.Instance.GridYLength];
-            }
         }
 
         // Update is called once per frame
         void Update()
         {
+            if (!NodeManager.Instance.SetupCompletate)
+                return;
 
+            VisualUpdateTiming += Time.deltaTime;
+            if(VisualUpdateTiming > VisualUpdateTimingInterval)
+            {
+                SightRangeScan();
+                VisualUpdateTiming = 0;
+            }
         }
 
-
-        //Maybe instead of calling as foreach loop... perhaps do a scan based on a function that retuns a array list of nodes based on the radius of vision... so 10...
-        void InitialSightRangeScan()
+        //
+        public void SightRangeScan()
         {
+            NodesInCurrentSight.Clear();
             Node InitialPos = NodeManager.Instance.FindNodeFromWorldPosition(transform.position);
-            Vector2Int InitialGridPos = InitialPos.GridPostion;
+            CurrentGridPos = InitialPos.GridPostion;
 
             //Toggles Bulk of Vision Area
-            for (int DepthXIndex = -(VisionRange - 1); DepthXIndex < VisionRange - 1; ++DepthXIndex)
+            for (int DepthXIndex = -(VisionRange); DepthXIndex <= VisionRange; ++DepthXIndex)
             {
-                for (int DepthYIndex = -(VisionRange - 1); DepthYIndex < VisionRange - 1; ++DepthYIndex)
+                for (int DepthYIndex = -(VisionRange); DepthYIndex <= VisionRange; ++DepthYIndex)
                 {
-                    int GridXPlusOffset = InitialGridPos.X + DepthXIndex;
-                    int GridYPlusOffset = InitialGridPos.Y + DepthYIndex;
-                    KnownNodes[GridXPlusOffset, GridYPlusOffset] = true;
-                }
-            }
-
-            //Vector2Int TopLeft =
-    
+                    if (Mathf.Abs(DepthXIndex) - Mathf.Abs(DepthYIndex) == 0 && (Mathf.Abs(DepthXIndex) == (VisionRange - 1) || Mathf.Abs(DepthYIndex) == (VisionRange - 1)))
+                        continue;
+                    if (Mathf.Abs(DepthXIndex) + Mathf.Abs(DepthYIndex) > (VisionRange + 1))
+                        continue;
 
 
+                    int GridXPlusOffset = CurrentGridPos.X + DepthXIndex;
+                    int GridYPlusOffset = CurrentGridPos.Y + DepthYIndex;
+                    GridXPlusOffset = Mathf.Clamp(GridXPlusOffset, 0, NodeManager.Instance.GridXLength);
+                    GridYPlusOffset = Mathf.Clamp(GridYPlusOffset, 0, NodeManager.Instance.GridYLength);
 
 
-
-        for (int DepthIndex = 0; DepthIndex < VisionRange; ++DepthIndex)
-            {
-                if (DepthIndex != VisionRange - 1)
-                {
-
-                }
-                if (DepthIndex == VisionRange - 1)
-                {
-
+                    if (NodeManager.Instance.NodeGrid[GridXPlusOffset, GridYPlusOffset] == null)
+                        continue;
+                    else
+                    {
+                        KnownNodes[GridXPlusOffset, GridYPlusOffset] = true;
+                        NodesInCurrentSight.Add(NodeManager.Instance.NodeGrid[GridXPlusOffset, GridYPlusOffset]);
+                    }
                 }
             }
         }
 
+        //These Visual Debuggings are really really costly and inefficent!!
+        private void OnDrawGizmos()
+        {
+            if (VisualKnowledgeDebugging)
+            {
+                if (!NodeManager.Instance.SetupCompletate)
+                    return;
+
+                for (int XIndex = 0; XIndex < NodeManager.Instance.GridXLength; ++XIndex)
+                    for (int YIndex = 0; YIndex < NodeManager.Instance.GridYLength; ++YIndex)
+                        if (KnownNodes[XIndex, YIndex] == true)
+                        {
+                            Gizmos.color = Color.magenta;
+                            Gizmos.DrawWireCube(NodeManager.Instance.NodeGrid[XIndex, YIndex].WorldPosition, NodeManager.Instance.NodeGrid[XIndex, YIndex].TileSize / 2);
+                        }
+
+            }
+            if (VisualSightDebugging)
+            {
+                if (!NodeManager.Instance.SetupCompletate)
+                    return;
+
+                for (int SightIndex = 0; SightIndex < NodesInCurrentSight.Count; ++SightIndex)
+                {
+                    if (NodesInCurrentSight[SightIndex] != null)
+                    {
+                        Gizmos.color = Color.magenta;
+                        Gizmos.DrawWireCube(NodesInCurrentSight[SightIndex].WorldPosition, NodesInCurrentSight[SightIndex].TileSize / 4);
+                    }
+                }
+            }
+        }
     }
-
-
-}
+ }
 
