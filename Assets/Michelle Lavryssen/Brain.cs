20da66244f435d4b645778afa639fb93b2194f6a
michelle lavryssen <1010199@student.sae.edu.au>
2018-06-13 16:41:17 +1000

    Created behavior tree

---
+++ Assets/Michelle Lavryssen/Brain.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Michelle
+{
+    public class Brain : CharacterBase
+    {
+
+        public bool isEnemyNear;
+        public int health;
+        public int energy;
+
+        public override void Ability1()
+        {
+            base.Ability1();
+            debugText = "Im siphoning health";
+        }
+
+        public override void Ability2()
+        {
+            base.Ability2();
+            debugText = "I'm raising undead";
+        }
+        public override void Ability3()
+        {
+            base.Ability3();
+            debugText = "I'm creating a bone shield between myself and the enemy";
+        }
+
+    }
+}
+    
