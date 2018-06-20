c6a9810b18f2cab9189c58124084b631774f86e5
Unknown <rhysipod@gmail.com>
2018-06-20 17:13:11 +1000

    21/6 Studio

--- Assets/Rhys Buszko/Scripts/AI/Controller.cs
+++ Assets/Rhys Buszko/Scripts/AI/Controller.cs
@@ -19,6 +19,8 @@ namespace Rhys
         public RaycastHit hitLeft;
         public RaycastHit hitRight;
 
+        public Manager manager;
+
         List<Collider> Enemys;
 
         Health My_Health;
@@ -34,6 +36,7 @@ namespace Rhys
             My_Health.OnDeathEvent += Death;
             My_Body = GetComponent<Body>();
             My_Body.Initalize_Me();
+            manager = Manager.Instance;
         }
 
         // Update is called once per frame
---
+++ Assets/Rhys Buszko/Scripts/Pathmanager/Manager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Rhys
+{
+    public class Manager : Singleton<Manager>
+    {
+        protected Manager() { } // guarantee this will be always a singleton only - can't use the constructor!
+
+        public string myGlobalVar = "whatever";
+        private Collider bounding;
+
+        public GameObject[,] map;
+
+        public GameObject node = new GameObject("node");
+
+        private void Start()
+        {
+            GameObject[] setup = FindObjectsOfType<GameObject>();
+
+            foreach(GameObject n in setup)
+            {
+                if(n.tag == "Bounds")
+                {
+
+                    bounding = n.GetComponent<Collider>() ;
+                }
+            }
+
+
+
+              int length = (int) bounding.bounds.size.x ;
+              int height = (int) bounding.bounds.size.z;
+            
+
+            map = new GameObject[length, height];
+
+            for(int i = 0; i < length; i++)
+            {
+                for(int j = 0; j < height; j++)
+                {
+                    map[i, j] = Instantiate(node, new Vector3(i, 1, j), new Quaternion(0, 0, 0, 0));
+                    print(map[i, j]);
+                }
+            }
+        }
+    }
+}
