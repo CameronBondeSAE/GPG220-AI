4045e9b9356c3a7590e9250db5e868006c1fc354
michelle lavryssen <1010199@student.sae.edu.au>
2018-06-27 05:05:09 +1000

    Added spawning of undead minions, reworked behavior tree

--- Assets/Michelle Lavryssen/Brain.cs
+++ Assets/Michelle Lavryssen/Brain.cs
@@ -4,30 +4,57 @@ using UnityEngine;
 
 namespace Michelle
 {
-    public class Brain : CharacterBase
-    {
+	public class Brain : CharacterBase
+	{
 
-        public bool isEnemyNear;
-        public int health;
-        public int energy;
+		public bool isEnemyNear;
+		public GameObject Necromancer;
+		public GameObject[] undead;
+		public Vector3 myPosition;
 
-        public override void Ability1()
-        {
-            base.Ability1();
-            debugText = "Im siphoning health";
-        }
 
-        public override void Ability2()
-        {
-            base.Ability2();
-            debugText = "I'm raising undead";
-        }
-        public override void Ability3()
-        {
-            base.Ability3();
-            debugText = "I'm creating a bone shield between myself and the enemy";
-        }
+		void Update ()
+		{
+			myPosition = GameObject.Find ("Necromancer").transform.position;
 
-    }
-}
-    
+		}
+
+		public override void Ability1 ()
+		{
+			base.Ability1 ();
+			debugText = "Im siphoning health";
+		}
+
+		public override void Ability2 ()
+		{
+			base.Ability2 ();
+			debugText = "I'm raising undead";
+			instantiateUndead ();
+
+
+		}
+
+		public override void Ability3 ()
+		{
+			base.Ability3 ();
+			debugText = "I'm creating a bone shield between myself and the enemy";
+		}
+			
+
+		public void instantiateUndead ()
+		{
+			GameObject[] clone = undead;
+			Vector3 UndeadSpawnPosition = new Vector3 (myPosition.x + Random.Range (-10f, 10f), myPosition.y, myPosition.z + Random.Range (-10f, 10f));
+			Instantiate (clone [0], UndeadSpawnPosition, Quaternion.identity);
+			Instantiate (clone [1], UndeadSpawnPosition, Quaternion.identity);
+			Instantiate (clone [2], UndeadSpawnPosition, Quaternion.identity);
+			Instantiate (clone [3], UndeadSpawnPosition, Quaternion.identity);
+		Instantiate (clone [4], UndeadSpawnPosition, Quaternion.identity);
+			//instantiates undead minions and then destroys them after 10 seconds
+		}
+			
+		
+
+		}
+	}
+	  
---
+++ Assets/Michelle Lavryssen/UndeadTimer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UndeadTimer : MonoBehaviour {
+	public float targetTime;
+	// Use this for initialization
+	public void randomizePositions()
+	{
+		gameObject.transform.position += new Vector3(Random.Range (-10f, 10f), 0, Random.Range (-10f, 10f));
+	}
+
+	void Start ()
+	{
+		randomizePositions ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+		targetTime -= Time.deltaTime;
+
+	}
+		
+
+}
